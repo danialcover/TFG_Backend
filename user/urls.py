c5bb31705d2bfe3be5bae19206cbdd15d5d02a5b
danielalcover1 <1562889@uab.cat>
2023-04-10 14:11:57 +0200

    creada api per eliminar profile

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import profile_create
+from .views import profile_create, profile_delete
 
 urlpatterns = [
-    path('profiles/role/<int:role_id>/', views.ProfileListByRole.as_view(), name='profile-list-by-role'),
-    path('profiles/create/', profile_create, name='profile-create')
+    path('profiles/<int:role_id>/', views.ProfileListByRole.as_view(), name='profile-list-by-role'),
+    path('profiles/create/', profile_create, name='profile-create'),
+    path('profiles/<int:pk>/delete/', profile_delete, name='profile-delete')
 ]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Profile
@@ -19,10 +21,27 @@ class ProfileListByRole(generics.ListAPIView):
 
 @csrf_exempt
 @api_view(['POST'])
-@permission_classes([AllowAny])
+@permission_classes([AllowAny, IsAuthenticated])
 def profile_create(request):
     serializer = ProfileSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def profile_delete(request, pk):
+    try:
+        profile = Profile.objects.get(pk=pk)
+    except Profile.DoesNotExist:
+        raise Http404("Profile not found")
+
+    if request.user == profile.user or request.user.is_superuser:
+        user = profile.user
+        profile.delete()
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response({"detail": "You do not have permission to delete this profile."}, status=status.HTTP_403_FORBIDDEN)
